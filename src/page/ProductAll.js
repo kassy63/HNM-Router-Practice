6b73ccbbbeedec6bf91d8c3f9f769af9286f81b9
kassy <sky122413@gmail.com>
2024-08-23 01:22:12 +0900

    코드 수정

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ProductCard from "../component/ProductCard";
 import { Col, Container, Row } from "react-bootstrap";
 
