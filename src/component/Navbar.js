d3218ea10d1218df9967bd87e7718d786672af6e
kassy <sky122413@gmail.com>
2024-08-23 19:39:00 +0900

    검색 및 반응형 사이드바 구현

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,12 +1,14 @@
 import "../App.css";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
   const navigate = useNavigate();
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const menuList = [
     "Women",
@@ -22,6 +24,7 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
   const goToHome = () => {
     navigate("/");
   };
+
   const handleAuthAction = () => {
     if (authenticate) {
       setAuthenticate(false); // 로그아웃 처리
@@ -31,23 +34,51 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
     }
   };
 
+  const search = (event) => {
+    if (event.key === "Enter") {
+      // 입력한 검색어를 읽어와서
+      const keyword = event.target.value;
+
+      // url을 바꿔준다
+      navigate(`/?q=${keyword}`);
+    }
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div>
-      <div>
-        <div className="login-button">
-          <FontAwesomeIcon icon={faUser} />
-          <Button variant="light" onClick={handleAuthAction}>
-            {authenticate ? "로그아웃" : "로그인"}
-          </Button>
-        </div>
+      <div className="login-button">
+        <FontAwesomeIcon icon={faUser} />
+        <Button variant="light" onClick={handleAuthAction}>
+          {authenticate ? "로그아웃" : "로그인"}
+        </Button>
       </div>
       <div className="nav-section">
         <img
+          className="logo"
           width={100}
-          style={{ cursor: "pointer" }}
           src="https://upload.wikimedia.org/wikipedia/commons/5/53/H%26M-Logo.svg"
           onClick={goToHome}
         />
+        <div className="hamburger-menu" onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+      </div>
+
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <div className="close-button" onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faTimes} />
+        </div>
+        <ul className="sidebar-menu-list">
+          {menuList.map((menu, index) => (
+            <li key={index} onClick={toggleSidebar}>
+              {menu}
+            </li>
+          ))}
+        </ul>
       </div>
 
       <div className="menu-area">
@@ -59,7 +90,11 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
 
         <div className="search-bar">
           <FontAwesomeIcon icon={faSearch} />
-          <input type="text" placeholder="검색" />
+          <input
+            type="text"
+            placeholder="검색"
+            onKeyPress={(event) => search(event)}
+          />
         </div>
       </div>
     </div>
